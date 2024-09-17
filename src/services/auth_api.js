a9229c61b29fdf46f99a96be2d02c24900dc34a3
Rishi <rishi.helloworld@gmail.com>
2024-09-17 22:51:49 +0530

    Fixed logout endpoint

--- src/services/auth_api.js
+++ src/services/auth_api.js
@@ -41,7 +41,7 @@ class Auth extends Api{
 
     async logout(){
         try {
-            this.Api.post('api/auth/logout'); // this sets the max-age of jwt-key to 0.
+            this.Api.post('api/logout'); // this sets the max-age of jwt-key to 0.
             return true;
         } catch (error) {
             console.error(error.message);
